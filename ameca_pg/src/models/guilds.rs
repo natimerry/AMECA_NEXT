0340c8afacaad9ba02bc061144486d2c6bd2cc37
soham nandy <soham.nandy2006@gmail.com>
2024-10-01 17:18:50 +0530

    null guild id entry (for now)

--- ameca_pg/src/models/guilds.rs
+++ ameca_pg/src/models/guilds.rs
@@ -11,7 +11,7 @@ use crate::BoxResult;
 #[derive(FromRow, Debug)]
 pub struct Guilds {
     pub guild_id: i64,
-    pub join_date: DateTime<chrono::Utc>,
+    pub join_date: DateTime<Utc>,
     pub members: i32,
 }
 
@@ -23,10 +23,10 @@ impl GuildData for Pool<Postgres> {
     async fn joined_guild(db: &Pool<Postgres>, members: i32, guild_id: GuildId) -> BoxResult<()>{
         info!("Registering new guild in database");
         let _guild = sqlx::query!(
-            "INSERT INTO guild (guild_id,members,join_date) VALUES ($1,$2,$3) ON CONFLICT DO NOTHING",
+            "INSERT INTO guild (guild_id,members,join_date) VALUES ($1,$2,$3::TIMESTAMPTZ) ON CONFLICT DO NOTHING",
             guild_id.get() as i64,
             members,
-            Utc::now().naive_utc()
+            Utc::now()
         ).execute(db).await.unwrap();
 
         debug!("guild insertion result: {:?}", _guild);
