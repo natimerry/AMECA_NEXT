fe1ef055ec6238db6f560918086c4d60cb9faaec
soham nandy <soham.nandy2006@gmail.com>
2024-10-01 20:28:34 +0530

    setup poise framework

---
+++ ameca_pg/src/bot.rs
@@ -0,0 +1,69 @@
+use crate::{BoxResult, DynError};
+use poise::builtins::register_globally;
+use poise::serenity_prelude as serenity;
+use serenity::all::{Message, Settings};
+use sqlx::{PgPool, Pool, Postgres};
+use tracing::{error, info};
+use crate::models::messasges::MessageData;
+
+pub struct AMECA {
+    db: Pool<Postgres>,
+}
+
+impl AMECA {
+    async fn event_handler(
+        ctx: &serenity::Context,
+        event: &serenity::FullEvent,
+        _framework: poise::FrameworkContext<'_, AMECA, DynError>,
+        data: &AMECA,
+    ) -> BoxResult<()> {
+        match event {
+            serenity::FullEvent::Message { new_message } => {
+                info!("New message: {} <{}>:{}", new_message.author.name,new_message.id,new_message.content);
+                let channel = new_message.channel(&ctx.http).await?;
+                let res = PgPool::new_message(&data.db, new_message.clone(), channel.guild().unwrap()).await;
+
+                if let Err(e) = res{
+                    error!("Unable to store message in db: {}", e);
+                }
+            },
+            _ => (),
+        }
+        Ok(())
+    }
+
+    pub async fn stard_shard(token: String,db: Pool<Postgres>) -> BoxResult<()> {
+        let mut settings = Settings::default();
+        settings.max_messages = 10000;
+
+        let framework = poise::Framework::builder()
+            .setup(|ctx, _ready, _framework| {
+                Box::pin(async move {
+                    register_globally(ctx, &_framework.options().commands).await?;
+                    Ok(AMECA { db })
+                })
+            })
+            .options(poise::FrameworkOptions {
+                commands: vec![],
+                event_handler: |ctx, event, framework, data| {
+                    Box::pin(AMECA::event_handler(ctx, event, framework, data))
+                },
+                ..Default::default()
+            })
+            .build();
+        let intents = serenity::GatewayIntents::AUTO_MODERATION_CONFIGURATION
+            | serenity::GatewayIntents::GUILD_MESSAGES
+            | serenity::GatewayIntents::GUILD_MESSAGE_REACTIONS
+            | serenity::GatewayIntents::AUTO_MODERATION_EXECUTION
+            | serenity::GatewayIntents::privileged();
+
+        let client = serenity::ClientBuilder::new(token, intents)
+            .framework(framework)
+            .cache_settings(settings)
+            .await;
+
+        client.unwrap().start().await?;
+
+        Ok(())
+    }
+}
--- ameca_pg/src/main.rs
+++ ameca_pg/src/main.rs
@@ -1,25 +1,42 @@
-use sqlx::Error::Database;
-use tracing::Level;
+use sqlx::{Pool, Postgres};
 use tracing::level_filters::LevelFilter;
+use tracing::{info, Level};
 use tracing_subscriber::fmt::writer::MakeWriterExt;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::EnvFilter;
+use crate::bot::AMECA;
 
-mod db;
+mod models;
+mod bot;
 
 type DynError = Box<dyn std::error::Error + Send + Sync>;
 type BoxResult<T> = Result<T, DynError>;
+
+
+pub async fn database_init() -> BoxResult<Pool<Postgres>> {
+    let url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    info!("Connecting to Postgres at {}", &url);
+    let pool = sqlx::postgres::PgPoolOptions::new()
+        .max_connections(256)
+        .connect(&url)
+        .await?;
+
+    info!("Running migrations");
+    sqlx::migrate!("./migrations").run(&pool).await?;
+
+    Ok(pool)
+}
 
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().expect("Failed to read .env file");
 
     let debug_file =
-        tracing_appender::rolling::hourly("./logs/", "debug").with_max_level(tracing::Level::TRACE);
+        tracing_appender::rolling::hourly("./logs/", "debug").with_max_level(Level::TRACE);
 
     let warn_file = tracing_appender::rolling::hourly("./logs/", "warnings")
-        .with_max_level(tracing::Level::WARN);
+        .with_max_level(Level::WARN);
     let all_files = debug_file.and(warn_file);
 
     tracing_subscriber::registry()
@@ -43,5 +60,8 @@ async fn main() {
         )
         .init();
 
-    let db = db::database::Database::init().await.expect("db init failed");
+    let db = database_init().await.expect("db init failed");
+
+    let token = std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN");
+    AMECA::stard_shard(token, db).await.expect("Error starting shard");
 }
