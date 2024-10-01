ae9197a062f1fa798823ab046735ba2d20e8fd90
soham nandy <soham.nandy2006@gmail.com>
2024-10-01 20:28:17 +0530

    create models.rs

---
+++ ameca_pg/src/models/messasges.rs
@@ -0,0 +1,58 @@
+use crate::BoxResult;
+use poise::serenity_prelude as serenity;
+use serenity::all::GuildChannel;
+use sqlx::types::chrono::{DateTime, Utc};
+use sqlx::{FromRow, Pool, Postgres};
+use std::future::Future;
+use tracing::{debug, info};
+
+#[derive(FromRow, Debug)]
+pub struct Message {
+    msg_id: i64,
+    content: String,
+    time: DateTime<Utc>,
+    author_id: i64,
+}
+
+pub trait MessageData {
+    fn new_message(
+        db: &Pool<Postgres>,
+        msg: serenity::Message,
+        channel: GuildChannel,
+    ) -> impl std::future::Future<Output = BoxResult<()>> + Send;
+
+    fn fetch_messages(
+        db: &Pool<Postgres>,
+        channel: GuildChannel,
+    ) -> impl std::future::Future<Output = BoxResult<Option<Message>>> + Send;
+}
+
+impl MessageData for Pool<Postgres> {
+    async fn new_message(
+        db: &Pool<Postgres>,
+        msg: serenity::all::Message,
+        channel: GuildChannel,
+    ) -> BoxResult<()> {
+        let msg_id = msg.id.get() as i64;
+        let msg_content = msg.content;
+        let msg_time = msg.timestamp.naive_utc();
+
+
+
+        sqlx::query!(
+            "INSERT INTO message(msg_id, content, time, author_id) VALUES ($1, $2, $3, $4)",
+            msg_id,
+            msg_content,
+            msg_time,
+            msg.author.id.get() as i64
+        ).execute(db).await?;
+        debug!("Created new message {}", msg_id);
+        Ok(())
+    }
+    async fn fetch_messages(
+        db: &Pool<Postgres>,
+        channel: GuildChannel,
+    ) -> BoxResult<Option<Message>> {
+        unimplemented!()
+    }
+}
